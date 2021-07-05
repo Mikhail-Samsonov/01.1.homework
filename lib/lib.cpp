690abf221386dd5d2f5d48cc4a3840161dee961b
Mikhail-Samsonov <mesamsonov@mail.ru>
2021-07-05 06:53:06 +0300

    first commit

---
+++ lib/lib.cpp
@@ -0,0 +1,5 @@
+#include "../lib/lib.h"
+
+#include "../lib/version.h"
+
+int version() { return PROJECT_VERSION_PATCH; }
---
+++ test/test_version_gtest.cpp
@@ -0,0 +1,5 @@
+#include "../lib/lib.h"
+
+#include <gtest/gtest.h>
+
+TEST(Version, Positive) { ASSERT_GT(version(), 0); }
